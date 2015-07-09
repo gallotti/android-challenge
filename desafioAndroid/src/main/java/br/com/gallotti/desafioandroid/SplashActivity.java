19724df6940713adffb6f29ce48777a1ea32a419
unknown <bruno@D779.reconcavotecnologia.org.br>
2015-07-09 18:35:21 -0300

    Melhorias

---
+++ desafioAndroid/src/main/java/br/com/gallotti/desafioandroid/SplashActivity.java
@@ -0,0 +1,7 @@
+package br.com.gallotti.desafioandroid;
+
+/**
+ * Created by bruno on 09/07/2015.
+ */
+public class SplashActivity {
+}
---
+++ desafioAndroid/src/main/java/br/com/gallotti/desafioandroid/util/SimpleLog.java
@@ -0,0 +1,7 @@
+package br.com.gallotti.desafioandroid.util;
+
+/**
+ * Created by bruno on 09/07/2015.
+ */
+public class SimpleLog {
+}
